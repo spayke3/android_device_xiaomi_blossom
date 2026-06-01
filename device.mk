500cfb73f860f66cc4157f5abbf9c117468616cc
Ulfa Sanjaya <spaykedotimas892@gmail.com>
2026-06-02 06:38:47 +0800

    fix building error on RevampedFMRadio

--- device.mk
+++ device.mk
@@ -158,16 +158,14 @@ PRODUCT_PACKAGES += \
     android.hardware.light-service.blossom
 
 # FM Radio
-ifeq ($(TARGET_INCLUDE_REVAMPED),true)
+
 PRODUCT_PACKAGES += \
-    RevampedFMRadio
-endif
+    $(if $(filter true,$(TARGET_INCLUDE_REVAMPED)),RevampedFMRadio)
 
 #Include ViaBrowser
-ifeq ($(TARGET_INCLUDE_VIA),true)
+
 PRODUCT_PACKAGES += \
-      ViaBrowser
-endif
+    $(if $(filter true,$(TARGET_INCLUDE_VIA)),ViaBrowser)
     
 # Display
 TARGET_SCREEN_DENSITY := 320
